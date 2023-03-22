0f0359e230cb77415bc496b1229b277fc9e5109e
llSilverDev <67278780+lSilverDev@users.noreply.github.com>
2023-03-22 09:31:51 -0300

    count - seconds

--- src/app/components/countdown/countdown.component.ts
+++ src/app/components/countdown/countdown.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./countdown.component.css']
 })
 export class CountdownComponent {
+  counter: number = 60;
 
+  ngOnInit(): void{
+    this.timer()
+  }
+
+  timer() {
+    let intervalId = setInterval(() => {
+        this.counter = this.counter - 1;
+        console.log(this.counter)
+        if(this.counter === 0) clearInterval(intervalId)
+    }, 1000)
+  }
 }
